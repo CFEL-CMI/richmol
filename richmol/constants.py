5c62d09edb470c0b58e3b86ccee849dae0f06f05
yachmena <andrey.yachmenev@desy.de>
2021-03-28 11:55:05 +0200

    Move constants module  from richmol/rot to richmol

--- richmol/constants.py
+++ richmol/constants.py
@@ -1,5 +1,5 @@
-planck     =  6.62606896e-27            # Planck constant
-avogno     =  6.0221415e+23             # Avogadro constant
-vellgt     =  2.99792458e+10            # Speed of light constant
-boltz      =  1.380658e-16              # Boltzmann constant
-bohr       =  0.529177249               # a.u.
+au_length = 0.5291772083    # atomic unit of length in Angstrom
+planck = 6.62606896e-27     # Plank constant in erg a second
+avogno = 6.0221415e+23      # Avogadro constant
+vellgt = 2.99792458e+10     # Speed of light constant in centimetres per second
+boltz = 1.380658e-16        # Boltzmann constant in erg per Kelvin
