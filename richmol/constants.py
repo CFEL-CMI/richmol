11394209e1a05a2d8e491d48fb7428b236c2ee44
yachmena <andrey.yachmenev@desy.de>
2021-03-31 23:30:37 +0200

    Delete constants module

--- richmol/constants.py
+++
@@ -1,5 +0,0 @@
-au_length = 0.5291772083    # atomic unit of length in Angstrom
-planck = 6.62606896e-27     # Plank constant in erg a second
-avogno = 6.0221415e+23      # Avogadro constant
-vellgt = 2.99792458e+10     # Speed of light constant in centimetres per second
-boltz = 1.380658e-16        # Boltzmann constant in erg per Kelvin
