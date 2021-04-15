63539307de63929b78a0871efe02213aa321dc47
yachmena <andrey.yachmenev@desy.de>
2021-04-15 19:08:49 +0200

    Test external field potential units

--- tests/benchmarks/h2o/test.py
+++ tests/benchmarks/h2o/test.py
@@ -1,4 +1,5 @@
 from richmol.rot.rchm import read_states, read_trans, add_tensor
+from richmol.convert_units import DebyeVm_to_invcm
 import json
 
 
@@ -13,8 +14,19 @@ def test_old_format(filename_states, filename_trans):
     add_tensor('random_tensor.h5', tens, replace=True)
 
 
+def stark_energies(states, trans):
+    """Computes Stark energies"""
+    h0 = read_states(states, jmin=0, jmax=5)
+    mu = read_trans(h0, trans)
+    field = [1,2,3]
+    mu.field(field)
+    mu.mul(-1.0)
+    fac = DebyeVm_to_invcm()
+    mu.mul(fac)
+
 if __name__ == '__main__':
     path = "tests/benchmarks/h2o/trove_rchm/"
     states = path + "energies_j0_j40_MARVEL_HITRAN.rchm"
     trans = path + "matelem_MU_j<j1>_j<j2>.rchm"
-    test_old_format(states, trans)
+
+    stark_energies(states, trans)
