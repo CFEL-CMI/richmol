f09559d69e5cc3df869c8c78acff027213aeb3b6
yachmena <andrey.yachmenev@desy.de>
2021-04-19 15:05:03 +0200

    Import Solutions from richmol.rot

--- richmol/rot/__init__.py
+++ richmol/rot/__init__.py
@@ -1,3 +1,3 @@
 from richmol.rot.molecule import Molecule, mol_tensor
-from richmol.rot.solution import solve
+from richmol.rot.solution import solve, Solution
 from richmol.rot.labtens import LabTensor
