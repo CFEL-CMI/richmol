495d9849336d7f2659235e6f9636fb971c430450
yachmena <andrey.yachmenev@desy.de>
2021-05-25 14:20:03 +0200

    Improve thresholds for matrix elements, updated docs

--- richmol/rot/labtens.py
+++ richmol/rot/labtens.py
@@ -44,13 +44,37 @@ class LabTensor(CarTens):
     Args:
         arg : numpy.ndarray, list or tuple, :py:class:`richmol.rot.Molecule`, or function
             Depending on the type of argument, initializes lab-frame molecular tensor operator
-            (arg is ndarray, list, or tuple), field-free Hamiltonian operator
+            (arg is numpy.ndarray, list, or tuple), field-free Hamiltonian operator
             (arg is :py:class:`richmol.rot.Molecule`), or arbitrary function of spherical
-            coordinates (arg is function(theta, phi)).
+            coordinates theta and phi (arg is function(theta, phi)).
         basis : :py:class:`richmol.rot.Solution`
             Rotational wave functions.
         thresh : float
             Threshold for neglecting matrix elements.
+
+    Kwargs:
+        bra, ket : function(**kw)
+            State filters for bra and ket basis sets, take as arguments state quantum numbers, symmetry,
+            and energy, i.e., `J`, `m`, `k`, `sym`, and `enr`, and return True or False depending on if
+            the corresponding state needs to be included or excluded form the basis.
+            By default, all states stored in the file are included.
+            The following keyword arguments are passed into the bra and ket functions:
+                J : float (round to first decimal)
+                    Value of J (or F) quantum number
+                sym : str
+                    State symmetry
+                enr : float
+                    State energy
+                m : str
+                    State assignment in the M subspace, usually just a value of the m quantum number
+                k : str
+                    State assignment in the K subspace, which are the rotational or ro-vibrational
+                    quanta joined in a string
+        wig_jmax : int
+            Maximal value of J quantum number used in the expansion of a user-defined function
+            of spherical coordinates in terms of Wigner D-functions.
+        leb_deg : int
+            Degree of the angular Lebedev quadrature used for computing the Wigner expansion coefficients.
     """
     # transformation matrix from Cartesian to spherical-tensor representation
     # for tensors of different ranks (dict keys)
@@ -102,24 +126,23 @@ class LabTensor(CarTens):
         else:
             raise TypeError(f"bad argument type for 'arg': '{type(arg)}'") from None
 
-        # initialize tensor attributes
+        # initialize tensor
 
         if tens is not None:
-            self.init_tens_from_rank(tens, basis.abc)
+            self.init_tens_from_rank(tens, basis.abc, thresh=thresh)
         elif func is not None:
-            self.init_tens_from_func(func, **kwargs)
+            self.init_tens_from_func(func, thresh=thresh, **kwargs)
 
         # matrix elements
 
-        self.kmat, self.mmat = self.matelem(basis, thresh)
+        self.kmat, self.mmat = self.matelem(basis, thresh=thresh)
 
         # for tensor representation of field-free Hamiltonian, make basis an attribute
 
         if isinstance(arg, Molecule):
             self.basis = basis
 
-        # for pure Cartesian tensor, keep in the attributes only basis set dimensions,
-        # basis state quanta and energies
+        # for pure Cartesian tensor, keep in the attributes only basis set dimensions and quanta
 
         Jlist = [round(float(J),1) for J in basis.keys()]
         symlist = {J : [sym for sym in basis[J].keys()] for J in basis.keys()}
@@ -130,15 +153,19 @@ class LabTensor(CarTens):
                   for sym, bas_sym in bas_J.items() }
                   for J, bas_J in basis.items() }
         dim = {J : {sym : dim_m[J][sym] * dim_k[J][sym] for sym in symlist[J]} for J in Jlist}
+
+        # use int(m) as m-assignment
         quanta_m = { J : { sym : [ int(elem[1]) for elem in bas_sym.m.table['stat'][:dim_m[J][sym]] ]
                      for sym, bas_sym in bas_J.items() }
                      for J, bas_J in basis.items() }
         try:
+            # use (k, energy) as k-assignment
             quanta_k = { J : { sym : [ (" ".join(q for q in elem[1:]), e)
                          for elem,e in zip(bas_sym.k.table['stat'][:dim_k[J][sym]], bas_sym.k.enr) ]
                          for sym, bas_sym in bas_J.items() }
                          for J, bas_J in basis.items() }
         except AttributeError:
+            # use (k, None) as k-assignment
             quanta_k = { J : { sym : [ (" ".join(q for q in elem[1:]), None)
                          for elem in bas_sym.k.table['stat'][:dim_k[J][sym]] ]
                          for sym, bas_sym in bas_J.items() }
@@ -163,9 +190,14 @@ class LabTensor(CarTens):
         self.filter(**kwargs)
 
 
-    def init_tens_from_rank(self, tens, abc):
+    def init_tens_from_rank(self, tens, abc, thresh=None):
         """Initializes components of the lab-frame tensor from the molecular-frame one
         """
+        if thresh is None:
+            thresh_ = np.finfo(np.complex128).eps
+        else:
+            thresh_ = thresh
+
         if not all(dim==3 for dim in tens.shape):
             raise ValueError(f"input tensor has inappropriate shape: '{tens.shape}' != {[3]*tens.ndim}") from None
         if np.any(np.isnan(tens)):
@@ -216,21 +248,29 @@ class LabTensor(CarTens):
                 self.Ux = np.delete(self.Ux, [1,2,3], 1)
                 self.os = [(omega,sigma) for (omega,sigma) in self.os if omega in (0,2)]
 
+        self.Us[np.abs(self.Us) < thresh_] = 0
+        self.Ux[np.abs(self.Ux) < thresh_] = 0
 
-    def init_tens_from_func(self, func, wig_jmax=100, wig_deg=131, wig_thresh=1e-12, **kwargs):
+
+    def init_tens_from_func(self, func, wig_jmax=100, leb_deg=131, thresh=None, **kwargs):
         """Initializes components of lab-frame tensor from arbitrary function `func`(theta, phi)
-        of theta and phi spherical angles
+        of spherical angles
 
         Expands function in terms of spherical-top functions |J,k=0,m> with J=0..`wig_jmax` and m=-J..J,
-        computes expansion coefficients <func|J,k=0,m> using Lebedev quadrature of degree `wig_deg`,
-        expansion coefficients smaller than `wig_thresh` are neglected
+        computes expansion coefficients <func|J,k=0,m> using Lebedev quadrature of degree `leb_deg`,
+        expansion coefficients smaller than `thresh` are neglected
         """
+        if thresh is None:
+            thresh_ = np.finfo(np.complex128).eps
+        else:
+            thresh_ = thresh
+
         self.rank = 0
         self.cart = ["0"]
         self.tens_flat = np.array([1], dtype=np.float64)
 
         # init Lebedev quadrature
-        quad_name = "lebedev_" + str(wig_deg).zfill(3)
+        quad_name = "lebedev_" + str(leb_deg).zfill(3)
         try:
             leb = quadpy.u3.schemes[quad_name]
         except KeyError:
@@ -253,7 +293,8 @@ class LabTensor(CarTens):
 
         for J in range(wig_jmax+1):
             ovlp[J] = ovlp[J] * 2*np.pi * np.sqrt((2*J+1)/(8*np.pi**2))
-            if np.all(np.abs(ovlp[J] < wig_thresh)):
+            ovlp[J][np.abs(ovlp[J]) < thresh_] = 0
+            if np.all(np.abs(ovlp[J] < thresh_)):
                 del ovlp[J]
 
         # initialize tensor Cartesian-to-spherical transformation
@@ -266,7 +307,7 @@ class LabTensor(CarTens):
             isigma = ind_sigma[(omega, sigma)]
             self.Ux[0, i] = ovlp[omega][isigma]
             if sigma == 0:
-                self.Us[i, 0] = 1.0
+                self.Us[i, 0] = 1
 
 
     def ham_proj(self, basis):
@@ -292,7 +333,7 @@ class LabTensor(CarTens):
         return res
 
 
-    def tens_proj(self, basis, maxJ=None, minJ=0, thresh=1e-14):
+    def tens_proj(self, basis, maxJ=None, minJ=0, thresh=None):
         """Computes action of tensor operator onto wave function
 
         Args:
@@ -305,7 +346,8 @@ class LabTensor(CarTens):
                 Minimal value of J quantum number allowed in tensor-projected wave functions.
             thresh : float
                 Threshold for considering elements of :py:attr:`Ux` and :py:attr:`Us` tensor
-                transformation matrices as zero.
+                transformation matrices as zero. If set to None, numpy-determined machine limit
+                for numpy.complex128 floating point type is used.
 
         Returns:
             res : nested dict
@@ -322,6 +364,11 @@ class LabTensor(CarTens):
             jk_table = basis.k.table
         except AttributeError:
             raise AttributeError(f"'{basis.__class__.__name__}' has no attribute 'k'") from None
+
+        if thresh is None:
+            thresh_ = np.finfo(np.complex128).eps
+        else:
+            thresh_ = thresh
 
         irreps = set(omega for (omega,sigma) in self.os)
         dj_max = max(irreps)    # selection rules |j1-j2|<=omega
@@ -360,7 +407,7 @@ class LabTensor(CarTens):
         # compute K|psi>
 
         UsT = {irrep : np.dot(self.Us[sigma_ind[irrep], :], self.tens_flat) for irrep in irreps}
-        ind = {irrep : np.where(np.abs(UsT[irrep]) > thresh)[0] for irrep in irreps}
+        ind = {irrep : np.where(np.abs(UsT[irrep]) > thresh_)[0] for irrep in irreps}
         nos = {irrep : len(ind[irrep]) for irrep in irreps if len(ind[irrep]) > 0}
         sig = {irrep : sigma[irrep][ind[irrep]] for irrep in nos.keys()}
         UsT = {irrep : UsT[irrep][ind[irrep]] for irrep in nos.keys()}
@@ -382,7 +429,7 @@ class LabTensor(CarTens):
         # compute M|psi>
 
         Ux = {irrep : self.Ux[:, sigma_ind[irrep]] for irrep in irreps}
-        ind = {irrep : list(set(i for i in np.where(np.abs(Ux[irrep]) > thresh)[1])) for irrep in irreps}
+        ind = {irrep : list(set(i for i in np.where(np.abs(Ux[irrep]) > thresh_)[1])) for irrep in irreps}
         nos = {irrep : len(ind[irrep]) for irrep in irreps if len(ind[irrep]) > 0}
         sig = {irrep : sigma[irrep][ind[irrep]] for irrep in nos.keys()}
 
@@ -417,7 +464,9 @@ class LabTensor(CarTens):
                 for different values of J quantum number and different symmetries,
                 i.e., basis[J][sym] -> SymtopBasis.
             thresh : float
-                Threshold for neglecting matrix elements.
+                Threshold for considering matrix elements as zero.
+                If set to None, numpy-determined machine limit for numpy.complex128 floating point
+                type is used.
 
         Returns:
             kmat : nested dict
@@ -425,6 +474,11 @@ class LabTensor(CarTens):
             mmat : nested dict
                 See :py:attr:`mmat`.
         """
+        if thresh is None:
+            thresh_ = np.finfo(np.complex128).eps
+        else:
+            thresh_ = thresh
+
         # dimensions of M and K tensors
 
         dim_m = { J: { sym : bas.m.table['c'].shape[1] for sym, bas in symbas.items() }
@@ -454,7 +508,7 @@ class LabTensor(CarTens):
                     proj = self.ham_proj(bas2)
                 except AttributeError:
                     # compute LabTensor|psi>
-                    proj = self.tens_proj(bas2, maxJ=maxJ, minJ=minJ)
+                    proj = self.tens_proj(bas2, maxJ=maxJ, thresh=thresh_)
 
                 for J1, symbas1 in basis.items():
 
@@ -463,7 +517,6 @@ class LabTensor(CarTens):
 
                     for sym1, bas1 in symbas1.items():
 
-                        print(J1, sym1, J2, sym2)
                         for irrep, bas_irrep in proj.items():
                             for cart, bas_cart in bas_irrep.items():
 
@@ -475,8 +528,7 @@ class LabTensor(CarTens):
                                     me = bas1.overlap_k(bas_cart)
 
                                     # set to zero elements with absolute values smaller than a threshold
-                                    if thresh is not None:
-                                        me[np.abs(me) < thresh] = 0
+                                    me[np.abs(me) < thresh_] = 0
                                     me_csr = csr_matrix(me)
 
                                     # check matrix dimensions
@@ -495,8 +547,7 @@ class LabTensor(CarTens):
                                 me = bas1.overlap_m(bas_cart)
 
                                 # set to zero elements with absolute values smaller than a threshold
-                                if thresh is not None:
-                                    me[np.abs(me) < thresh] = 0
+                                me[np.abs(me) < thresh_] = 0
                                 me_csr = csr_matrix(me)
 
                                 # check matrix dimensions
@@ -513,10 +564,21 @@ class LabTensor(CarTens):
 
 
     def class_name(self):
-        """Generates '__class_name__' attribute for the tensor data group in HDF5 file"""
+        """Generates string containing name of the parent class"""
         base = list(self.__class__.__bases__)[0]
         return base.__module__ + "." + base.__name__
 
+
+def cos2theta(theta, phi):
+    return np.cos(theta)**2
+
+
+def cos2theta2d(theta, phi):
+    tol = 1.0e-12
+    if abs(theta - np.pi/2.0) <= tol or abs(theta - 3.0*np.pi/2.0) <= tol:
+        if abs(phi) <= tol or abs(phi - np.pi) <= tol or abs(phi - 2.0*np.pi) <= tol:
+            return 1.0
+    return np.cos(theta)**2/(np.sin(theta)**2*np.sin(phi)**2 + np.cos(theta)**2)
 
 
 def retrieve_name(var):
