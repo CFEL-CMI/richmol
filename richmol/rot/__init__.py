cc80b133facbbac60d76e04f04feb1c0182f93d8
yachmena <andrey.yachmenev@desy.de>
2021-05-25 14:31:19 +0200

    Add cos2theta and cos2theta2 into rot.__init__

--- richmol/rot/__init__.py
+++ richmol/rot/__init__.py
@@ -1,3 +1,3 @@
 from richmol.rot.molecule import Molecule, mol_tensor
 from richmol.rot.solution import solve, Solution
-from richmol.rot.labtens import LabTensor
+from richmol.rot.labtens import LabTensor, cos2theta, cos2theta2d
