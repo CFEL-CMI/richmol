27c16a24a6dcc7419e4dc7a47445c56297502656
Andrey Yachmenev <53194930+yachmena@users.noreply.github.com>
2021-06-18 22:26:59 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,15 +10,10 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import mock
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
 sys.setrecursionlimit(1500)
-
-MOCK_MODULES = ['numpy', 'scipy', 'scipy.constants', 'scipy.sparse']
-for mod_name in MOCK_MODULES:
-    sys.modules[mod_name] = mock.Mock()
 
 # -- Project information -----------------------------------------------------
 
