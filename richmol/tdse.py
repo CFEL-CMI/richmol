55d9312ea2ef778376f09a0c0589281cc0a3d187
saribalc <cem.saribal@cfel.de>
2021-06-28 15:44:48 +0200

    Update `tdse.TDSE.update` function
    This commit is related to #20.
    
    Add keyword argument 'propag' to `tdse.TDSE.update` function for selecting the propagator. Set to 'internal' for in-house Lanczos or to `external` for expokit.
    
    Reduce signature of `tdse.TDSE.update` by shifting keyword argument handling into its body.
    
    Clean up.

--- richmol/tdse.py
+++ richmol/tdse.py
@@ -3,6 +3,7 @@ import scipy.constants as const
 import functools
 from scipy.sparse import diags
 from scipy.sparse.linalg import expm, onenormest
+import richmol
 from richmol import convert_units
 from richmol.pyexpokit import zhexpv
 
@@ -228,7 +229,7 @@ class TDSE():
             hmat = H.tomat(form="full", repres="dense")
         except AttributeError:
             raise AttributeError(
-                f"Hamiltonian `H` has bad type (must be Hamiltonian)"
+                f"hamiltonian `H` has bad type (must be Hamiltonian)"
             ) from None
         if H_is_diag:
             enrs = np.sort(np.einsum('ii->i', hmat))
@@ -265,28 +266,73 @@ class TDSE():
 
 
     @update_counter
-    def update(self, H, vecs, H0=None, matvec_lib='scipy', tol=1e-15):
+    def update(self, H, vecs, **kwargs):
         """ Propagates vectors by one time-step
 
             Args:
-                H : :py:class:`field.CarTens`
+                H : :py:class:`~richmol.field.CarTens`
                     Hamiltonian operator
                 vecs : list
                     Vectors to propagate in time
 
             Kwargs:
-                H0 : :py:class:`field.CarTens`
+                H0 : :py:class:`~richmol.field.CarTens`
                     Field-free Hamiltonian operator (default: None)
                 matvec_lib : str
-                    Library to use for matrix-vector products (default:
-                    'scipy')
+                    See :py:func:`~richmol.field.CarTens.vec`
+                propag : str
+                    The propagator to use during computations. Set to
+                    'internal' for in-house Lanczos implementation (see
+                    :py:func:`_expmv_lanczos`) or set to `external` for expokit
+                    (see :py:func:`~richmol.expokit.zhexpv`)
                 tol : int, float
-                    See :py:func:`_Lanczos_expmv`
-               
+                    See :py:func:`_expmv_lanczos`
+
             Returns:
                 vecs2 : list
                     Vectors propagated in time
         """
+
+        # field-free Hamiltonian
+        if 'H0' in kwargs:
+            assert (type(kwargs['H0']) is richmol.field.CarTens), \
+                f"field-free Hamiltonian `H0` has bad type: " \
+                    + f"'{type(kwargs['H0'])}', " \
+                    + f"(must be '{richmol.field.CarTens}')"
+            H0 = kwargs['H0']
+        else:
+            H0 = None
+
+        # matrix-vector product library
+        if 'matvec_lib' in kwargs:
+            matvec_lib = kwargs['matvec_lib']
+        else:
+            matvec_lib = 'scipy'
+
+        # propagator
+        if 'propag' in kwargs:
+            assert (type(kwargs['propag']) is str), \
+                f"propagator 'propag' has bad type: " \
+                    + f"'{type(kwargs['proag'])}', " \
+                    + f"(must be 'str')"
+            assert (kwargs['propag'] in ['external', 'internal']), \
+                f"propagator 'propag' has bad value: '{kwargs['propag']}', " \
+                    + f"(use 'external', 'internal')"
+            propag = kwargs['propag']
+        else:
+            propag = 'internal'
+
+        # tolerance
+        if 'tol' in kwargs:
+            assert (type(kwargs['tol']) in [int, float]), \
+                f"tolerance `tol` has bad type: '{type(kwargs['tol'])}', " \
+                    + f"(must be 'int', 'float')"
+            assert (kwargs['tol'] > 0 and kwargs['tol'] <= 1), \
+                f"tolerance `tol` has bad value: '{kwargs['tol']}', " \
+                    + f"(must be > 0 and <= 1)"
+            tol = kwargs['tol']
+        else:
+            tol = 1e-15
 
         # factor in exponent
         exp_fac = -1j * self._dt * self._t_to_s * self._enr_to_J \
@@ -319,27 +365,36 @@ class TDSE():
 
         # propagate
         if H0 is not None:
+            # use split-operator approach
+
             if '_exp_fac_H0' not in list(self.__dict__.keys()):
                 H0_mat = H0.tomat(form='full', cart='0')
                 assert ((H0_mat - diags(H0_mat.diagonal())).nnz == 0), \
-                    f"non-diagonal H0: split-operator approach not " \
-                        + f"applicable / implemented"
+                    f"field-free Hamiltonian `H0` is not diagonal -> " \
+                        + f"split-operator approach not implemented"
                 self._exp_fac_H0 = np.exp(exp_fac / 2 * H0_mat.diagonal())
+
             vecs2 = vecs * self._exp_fac_H0
+
             if hasattr(H, 'mfmat') and len(H.mfmat) > 0:
-                for ind, vec in enumerate(vecs2):
-                    vecs2[ind] = _expv_lanczos(
-                        vec, exp_fac, lambda v : cartensvec(v), tol=tol
-                    )
-                    # vecs2[ind] = zhexpv(
-                    #     vec,
-                    #     onenormest(H.tomat(form='full')),
-                    #     12,
-                    #     exp_fac.imag,
-                    #     lambda v : cartensvec(v) * 1j,
-                    #     tol = tol
-                    # )
+                if propag == 'internal':
+                    for ind, vec in enumerate(vecs2):
+                        vecs2[ind] = _expmv_lanczos(
+                            vec, exp_fac, lambda v : cartensvec(v), tol=tol
+                        )
+                else:
+                    for ind, vec in enumerate(vecs2):
+                        vecs2[ind] = zhexpv(
+                            vec,
+                            onenormest(H.tomat(form='full')),
+                            12,
+                            exp_fac.imag,
+                            lambda v : cartensvec(v) * 1j,
+                            tol = tol
+                        )
+
             vecs2 *= self._exp_fac_H0
+
         else:
             vecs2 = np.empty(vecs.shape, dtype=vecs.dtype)
             for ind, vec in enumerate(vecs):
@@ -350,7 +405,7 @@ class TDSE():
         return vecs2
 
 
-def _expv_lanczos(vec, fac, matvec, maxorder=100, tol=1e-15):
+def _expmv_lanczos(vec, fac, matvec, maxorder=100, tol=1e-15):
     """ Computes  epx(fac * H) * vec  using in-house Lanczos algorithm 
 
         Args:
@@ -364,19 +419,13 @@ def _expv_lanczos(vec, fac, matvec, maxorder=100, tol=1e-15):
                 Maximum order of Krylov subspace basis (default: 100)
             tol : int, float
                 Tolerance w.r.t. metric of two subsequent solutions below
-                which to stop expanding basis of Krylov subspace (default: 0)
+                which to stop expanding basis of Krylov subspace (default:
+                1e-15)
 
         Returns:
             u_k : numpy.ndarray
                 Numerical solution
     """
-
-    # tolerance
-    assert (type(tol) in [int, float]), \
-        f"Tolerance `tol` has bad type: '{type(tol)}', (must be 'int', " \
-            + f"'float')"
-    assert (tol > 0 and tol <= 1), \
-        f"Tolerance `tol` has bad value: '{tol}', (must be > 0 and <= 1)"
 
     V, W = [], []
     T = np.zeros((maxorder, maxorder), dtype=vec.dtype)
