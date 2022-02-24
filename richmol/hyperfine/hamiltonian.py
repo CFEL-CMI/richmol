0607c405f2b911b4cf82c2ec8e87691f3ad49cba
Andrey Yachmenev <andrey.yachmenev@desy.de>
2022-02-24 11:23:54 +0100

    Implement spin-rotation interaction, test results agree with HyFor for water

--- richmol/hyperfine/hamiltonian.py
+++ richmol/hyperfine/hamiltonian.py
@@ -1,5 +1,5 @@
 import numpy as np
-from reduced_me import spinMe_IxI
+from reduced_me import spinMe_IxI, spinMe
 from basis import nearEqualCoupling, spinNearEqualCoupling
 from richmol.field import CarTens
 import py3nj
@@ -70,13 +70,59 @@ def Hamiltonian(f, spins, h0, quad=None, sr=None, ss=None, eQ=None,
         allowedSym2[key] = [sym for sym in h0.symlist2[j] if symmetryRules(spin, sym)]
         print(spin, " %3i"%j, "  ", allowedSym1[key], "  ", allowedSym2[key])
 
+    # spin-rotation intermediates
+
+    if sr is not None:
+
+        tol = 1e-8
+        Nl = np.array([1, -np.sqrt(3), np.sqrt(5)], dtype=np.float64)
+        Nl1 = Nl * np.array([(-1)**l for l in range(3)])
+        Nl2 = Nl
+
+        if any(n < 0 or n > len(spins) for n in sr.keys()):
+            raise ValueError(
+                f"Bad spin indices in keys of 'sr' parameter = {list(sr.keys())}, " + \
+                f"these must be positive integers not exceeding total number of " + \
+                f"spins = {len(spins)}") from None
+
+        coef1 = np.zeros((len(jQuanta), len(jQuanta), 3), dtype=np.float64)
+        coef2 = np.zeros((len(jQuanta), len(jQuanta), 3), dtype=np.float64)
+
+        for i, (spin1, j1) in enumerate(zip(spinQuanta, jQuanta)):
+            for j, (spin2, j2) in enumerate(zip(spinQuanta, jQuanta)):
+                fac = spin2[-1] + f
+                assert (float(fac).is_integer()), f"Non-integer power in (-1)**f: '(-1)**{fac}'"
+                fac = int(fac)
+                prefac = (-1)**fac * np.sqrt((2*j1+1) * (2*j2+1)) \
+                       * py3nj.wigner6j(int(spin1[-1]*2), int(j1*2), int(f*2), 
+                                        int(j2*2), int(spin2[-1]*2), 2)
+                if j2 > 0:
+                    threej = py3nj.wigner3j(int(j2*2), 2, int(j2*2), -int(j2*2), 0, int(j2*2))
+                    if abs(threej) < tol:
+                        raise ValueError(f"can't divide by 3j-symbol (J 1 J)(-J 0 J) = {threej}") from None
+                    coef1[i, j, :] = prefac * j2 / threej * Nl1 \
+                                   * py3nj.wigner6j([l*2 for l in range(3)], [2]*3, [2]*3,
+                                                    [int(j2*2)]*3, [int(j1*2)]*3, [int(j2*2)]*3)
+                if j1 > 0:
+                    threej = py3nj.wigner3j(int(j1*2), 2, int(j1*2), -int(j1*2), 0, int(j1*2))
+                    if abs(threej) < tol:
+                        raise ValueError(f"can't divide by 3j-symbol (J' 1 J')(-J' 0 J') = {threej}") from None
+                    coef2[i, j, :] = prefac * j1 / threej * Nl1 \
+                                   * py3nj.wigner6j([2]*3, [l*2 for l in range(3)], [2]*3,
+                                                    [int(j2*2)]*3, [int(j1*2)]*3, [int(j1*2)]*3)
+
+        rme = spinMe(spinQuanta, spinQuanta, spins, 1, 'spin')
+        rme_sr = 0.5 * ( np.einsum('kij,ijl->klij', rme, coef1) \
+                       + np.einsum('kij,ijl->klij', rme, coef2) )
+
+
     # spin-spin intermediates
 
     if ss is not None:
 
         if any(n < 0 or n > len(spins) for n12 in ss.keys() for n in n12):
             raise ValueError(
-                f"Bad spin indices in the keys of 'ss' parameter = {list(ss.keys())}, " + \
+                f"Bad spin indices in keys of 'ss' parameter = {list(ss.keys())}, " + \
                 f"these must be positive integers not exceeding total number of " + \
                 f"spins = {len(spins)}") from None
 
@@ -140,9 +186,21 @@ def Hamiltonian(f, spins, h0, quad=None, sr=None, ss=None, eQ=None,
                         for (n1, n2) in ss.keys():
                             try:
                                 kmat = ss[(n1, n2)].kmat[(j1, j2)][(sym1, sym2)][ss_rank].toarray()
+                                mat += kmat * rme_ss[n1, n2, i, j]
                             except (AttributeError, KeyError):
                                 continue
-                            mat += kmat * rme_ss[n1, n2, i, j]
+
+                    # spin-rotation part
+
+                    if sr is not None:
+                        for n1 in sr.keys():
+                            kmat = sr[n1].kmat[(j1, j2)][(sym1, sym2)]
+                            for l, kmat_l in kmat.items():
+                                try:
+                                    mat += kmat_l.toarray() * rme_sr[n1, l, i, j]
+                                except AttributeError:
+                                    continue
+
                     hamMat_.append(mat)
 
             hamMat0.append(hamMat0_)
@@ -215,7 +273,13 @@ if __name__ == '__main__':
     richmolFile = "/gpfs/cfel/group/cmi/data/Theory_H2O_hyperfine/H2O-16/basis_p48/richmol_database_rovib/h2o_p48_j40_rovib.h5"
     h0 = CarTens(richmolFile, name='h0')
     ss = CarTens(richmolFile, name='spin-spin H1-H2')
+    sr1 = CarTens(richmolFile, name='spin-rot H1')
+    sr2 = CarTens(richmolFile, name='spin-rot H2')
 
-    ham, ham0 = Hamiltonian(f, spins, h0, ss={(0, 1): ss}, symmetryRules=c2vOrthoParaB1Rules)
-    enr, _ = np.linalg.eigh(ham0.real + ham * kHz_to_invcm)
+    ss *= kHz_to_invcm
+    sr1 *= -kHz_to_invcm
+    sr2 *= -kHz_to_invcm
+
+    ham, ham0 = Hamiltonian(f, spins, h0, ss={(0, 1): ss}, sr={0: sr1, 1: sr2}, symmetryRules=c2vOrthoParaB1Rules)
+    enr, _ = np.linalg.eigh(ham0 + ham)
     print([(e, (e-e0)/kHz_to_invcm) for e, e0 in zip(enr, np.sort(np.diag(ham0.real)))])
